b954d1fa541c53d6ead2dc2de29190b86774245e
MakoskiVictor <makoski.ed@gmail.com>
2023-05-16 20:05:10 -0300

    created types definitions

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+import { ImageURISource } from "react-native/types"
+
+export interface PopularMovies {
+  id: number
+  title: string
+  otiginal_title: string
+  original_language: string
+  poster_path: ImageURISource
+  vote_average: number
+}
