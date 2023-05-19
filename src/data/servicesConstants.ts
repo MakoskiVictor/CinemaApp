c894fd1e215664bd423854e7fab64957587f9c71
MakoskiVictor <makoski.ed@gmail.com>
2023-05-19 10:33:46 -0300

    added INIT_PATH

--- src/data/servicesConstants.ts
+++ src/data/servicesConstants.ts
@@ -3,3 +3,5 @@ export const API_URL = 'https://api.themoviedb.org/3'
 export const POPULAR_PATH = '/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'
 
 export const API_KEY = '&api_key=1fdf19c0ae38cd29cc36b8c814b47e6b'
+
+export const IMAGE_INIT_PATH = 'https://image.tmdb.org/t/p/original'
