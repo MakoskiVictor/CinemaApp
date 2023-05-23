111d416bf15a24b885fb51741459f77ac5a99bca
MakoskiVictor <makoski.ed@gmail.com>
2023-05-23 09:17:56 -0300

    added PopularMovies type

---
+++ api/src/types.d.ts
@@ -0,0 +1,9 @@
+export interface PopularMovies {
+  id: number
+  title: string
+  otiginal_title: string
+  original_language: string
+  poster_path: ImageURISource
+  vote_average: number
+  overview: string
+}
