f815b31b4d266c74731d53f279f9956ab846f1ea
MakoskiVictor <makoski.ed@gmail.com>
2023-05-23 09:17:41 -0300

    created constants

---
+++ api/src/data/constants.ts
@@ -0,0 +1,3 @@
+export const API_URL = 'https://api.themoviedb.org/3'
+
+export const POPULAR_PATH = '/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'
