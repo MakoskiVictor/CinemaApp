221d534c379fa8bf430c9db3ab964dc55d173b16
MakoskiVictor <makoski.ed@gmail.com>
2023-05-23 09:18:20 -0300

    added popular route

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,16 @@
 import { Hono } from 'hono'
 
+import { fetchMovies } from './controllers/popular'
+
 const app = new Hono()
 
 app.get('/', (c) => c.text('Hello Hono!'))
+
+app.get('/popular', async (c) => {
+  const res = await fetchMovies()
+  c.json(res)
+})
+
+app.notFound((c) => c.text('Not Found this route', 404))
 
 export default app
