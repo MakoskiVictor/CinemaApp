672e0f2a2aeff51c0dfd6b09e361fb51c94f64d5
MakoskiVictor <makoski.ed@gmail.com>
2023-05-30 10:01:36 -0300

    added movie path

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,6 +1,7 @@
 import { View, StyleSheet, ImageBackground } from 'react-native'
 import { Routes, Route } from 'react-router-native'
 import { PopularMovies } from './PopularMovies'
+import { Movie } from './Movie'
 import { NavBar } from '../components/NavBar'
 import Constants from 'expo-constants'
 
@@ -11,7 +12,10 @@ export function Main () {
       <NavBar />
       <ImageBackground source={require('../assets/backgrounds/gradient.jpg')} resizeMode="cover" style={styles.image}>
         <Routes >
-          <Route path='/' Component={PopularMovies }/>         
+          <Route path='/' Component={ PopularMovies }/>         
+        </Routes>
+        <Routes >
+          <Route path='/movie/:id' Component={ Movie }/>         
         </Routes>
         </ImageBackground>
     </View>
