59e74872b0f70594c6bed35c877ece93b52da573
MakoskiVictor <makoski.ed@gmail.com>
2023-05-30 10:21:54 -0300

    moved interfaces into type Movie folder

---
+++ client/src/types/movies/types.d.ts
@@ -0,0 +1,52 @@
+import { ImageURISource } from "react-native/types"
+
+export interface PopularMovies {
+  id: number
+  title: string
+  otiginal_title: string
+  original_language: string
+  poster_path: ImageURISource
+  vote_average: number
+  overview: string
+}
+
+export interface PopularMoviesChildren {
+  popularMovies: PopularMovies[];
+}
+
+export interface SingleMovie {
+  adult: boolean
+  genres: Genres
+  id: number
+  images: {
+    poster1: string
+    poster2: string
+    poster3: string
+    poster4: string
+  }
+  language: string
+  overview: string
+  release: string
+  score: number
+  status: string
+  tagline: string
+  title: string
+  total_votes: number
+}
+
+interface Genres {
+   [
+    {
+      id: number, 
+      name: string 
+    },
+    {
+      id: number, 
+      name: string
+    },
+    {
+      id: number,
+      name: string
+    }
+  ]
+}
