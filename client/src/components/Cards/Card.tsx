44863b657b57bcc57c6a97d8e1e5bb74129402f6
MakoskiVictor <makoski.ed@gmail.com>
2023-05-30 10:00:08 -0300

    linked card

--- client/src/components/Cards/Card.tsx
+++ client/src/components/Cards/Card.tsx
@@ -1,18 +1,20 @@
 import { StyleSheet, Text, View, FlatList, Image } from 'react-native'
 import { IMAGE_INIT_PATH } from '../../data/servicesConstants'
 import { type PopularMoviesChildren } from '../../types'
+import { Link } from 'react-router-native'
 
 export function Card ( {popularMovies} : PopularMoviesChildren): JSX.Element {
 
 
   return(
     <FlatList showsVerticalScrollIndicator={false} style={styles.flatList} data={popularMovies} renderItem={( {item} )  => (
-      
+      <Link to={`/movie/${item.id}`}>
       <View style={styles.card}  key={item.id}>
         <Image source={{uri: `${IMAGE_INIT_PATH}${item.poster_path}` }} style={styles.image} />
         <Text style={styles.title}>{item.title}</Text>
         <Text>Score: {item.vote_average} </Text>
         </View>
+        </Link>
     )} />
   )
 }
