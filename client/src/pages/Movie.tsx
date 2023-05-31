bde9513d6c55daca5a3576ec39e13e5753170875
MakoskiVictor <makoski.ed@gmail.com>
2023-05-31 10:57:05 -0300

    import detailed card

--- client/src/pages/Movie.tsx
+++ client/src/pages/Movie.tsx
@@ -1,16 +1,25 @@
-import { View, Text } from "react-native"
+import { View, StyleSheet } from "react-native"
 import { useParams } from 'react-router-native'
 import { useSearchMovie } from "../hooks/useSearchMovie"
+import { DetailedCard } from "../components/Cards/DetailedCard"
 
 export function Movie () {
 
   let { id } = useParams()
   const { movie, isLoading, error } = useSearchMovie(id)
 
-    console.log(movie)
+   console.log(movie)
   return(
-    <View>
-      <Text>MOVIE: {id} </Text>
+    <View style={styles.view}>
+      <DetailedCard singleMovie={movie} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  view: {
+    alignItems: 'center',
+    justifyItems: 'center'
+  }
+})
+
